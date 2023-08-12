914611a6940f45664217b2d4cededbdff437a031
Rolando S Munoz <38341442+rolmun@users.noreply.github.com>
2023-08-12 10:50:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -15,13 +15,16 @@ SLACK_BOT_TOKEN = os.environ.get("ACA_App_Bot_User_OAuth_Token")
 SLACK_APP_TOKEN = os.environ.get("ACA_App_Socket_Mode_Token")
 
 app = App(token=SLACK_BOT_TOKEN)
+@app.event("app_mention")
+
+def mention_handler(body, context, payload, options, say, event):
+    say("Hello World!")
 
 #test w/ friend
 @app.event("member_joined_channel")
 def welcome_msg(event,say):
     welcome_txt = f"Welcome <@{event['user']}> to the channel!"
     say(text=welcome_txt, channel=event['channel'])
-
 
 @app.event("message")
 def mention_handler(body, context, payload, options, say, event):
