15f9d49e5574bdd753a838a6ed068e611abe2bde
Rolando S Munoz <38341442+rolmun@users.noreply.github.com>
2023-08-13 15:28:59 -0700

    Give current time/date

--- main.py
+++ main.py
@@ -2,23 +2,26 @@ import os
 import logging
 
 # For Slack Bolt
-from pyexpat.errors import messages
-
-from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from datetime import datetime
 
 logging.basicConfig(level=logging.INFO)
+
+from dotenv import load_dotenv
 load_dotenv()
 
 SLACK_BOT_TOKEN = os.environ.get("ACA_App_Bot_User_OAuth_Token")
 SLACK_APP_TOKEN = os.environ.get("ACA_App_Socket_Mode_Token")
 
 app = App(token=SLACK_BOT_TOKEN)
-@app.event("app_mention")
 
+@app.event("app_mention")
 def mention_handler(body, context, payload, options, say, event):
-    say("Hello World!")
+    now = datetime.now()
+    current_time = now.strftime("%m/%d/%Y | %H:%M:%S")
+    say(f"Hi! The current time is {current_time}")
+
 
 #test w/ friend
 @app.event("member_joined_channel")
@@ -26,9 +29,6 @@ def welcome_msg(event,say):
     welcome_txt = f"Welcome <@{event['user']}> to the channel!"
     say(text=welcome_txt, channel=event['channel'])
 
-@app.event("message")
-def mention_handler(body, context, payload, options, say, event):
-    pass
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app, SLACK_APP_TOKEN)
